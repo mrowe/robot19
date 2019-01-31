d41c40fb5f5ce8cef60d5d5d6724314b1b3dd016
Michael Rowe <mrowe@mojain.com>
2019-01-31 15:47:54 +1100

    Implement Move command

--- src/main/java/com/mojain/robot19/commands/MoveCommand.java
+++ src/main/java/com/mojain/robot19/commands/MoveCommand.java
@@ -1,11 +1,29 @@
 package com.mojain.robot19.commands;
 
 import com.mojain.robot19.Command;
+import com.mojain.robot19.Position;
 import com.mojain.robot19.Robot;
 
 public class MoveCommand implements Command {
     @Override
     public Robot invoke(Robot robot) {
-        return robot;
+        Position potentialPos;
+        switch (robot.facing) {
+            case NORTH:
+                potentialPos = new Position(robot.position.x, robot.position.y + 1);
+                break;
+            case SOUTH:
+                potentialPos = new Position(robot.position.x, robot.position.y - 1);
+                break;
+            case EAST:
+                potentialPos = new Position(robot.position.x + 1, robot.position.y);
+                break;
+            case WEST:
+                potentialPos = new Position(robot.position.x - 1, robot.position.y);
+                break;
+            default:
+                potentialPos = robot.position;
+        }
+        return new Robot(potentialPos, robot.facing);
     }
 }
