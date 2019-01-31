83bfd6a461d423768b2bd9aea791b3b6f1d64427
Michael Rowe <mrowe@mojain.com>
2019-01-31 16:05:25 +1100

    Implement main control loop

--- src/main/java/com/mojain/robot19/ToyRobot.java
+++ src/main/java/com/mojain/robot19/ToyRobot.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 public class ToyRobot {
 
     public static void main(String[] args) {
+        Parser parser = new Parser();
         Scanner in = new Scanner(System.in);
+        Robot robot = new Robot(new Position(0, 0), Direction.NORTH);
 
         while (in.hasNext()) {
-            in.nextLine();
+            String input = in.nextLine();
+            Command command = parser.parse(input);
+            robot = command.invoke(robot);
         }
-
-        System.out.println("Hello, world");
     }
 }
